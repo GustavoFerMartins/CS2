33c7dd1c84101c0043a664c384f0afa5682f7e1d
GustavoFerMartins <60547697+Gustavo3022020@users.noreply.github.com>
2024-03-02 16:36:25 -0300

    Structuring for the future backend in Spring
    Structuring for the future backend in Spring.

---
+++ backend/src/main/java/com/cs2/steamcsgo/SteamCsGoApplication.java
@@ -0,0 +1,13 @@
+package com.cs2.steamcsgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamCsGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamCsGoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cs2/steamcsgo/SteamCsGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs2.steamcsgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamCsGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
