0103979bb3b17a76ef596cc721bdeacc4cefcc85
GustavoFerMartins <60547697+Gustavo3022020@users.noreply.github.com>
2024-03-02 22:34:42 -0300

    Update
    Update

---
+++ frontend/src/app/components/playerprofile.tsx
@@ -0,0 +1,50 @@
+import "../styles/playerprofile.css";
+
+export function PlayerProfile() {
+  return (
+
+    <div className="mt-[44px] sd:mt-[100px]">
+      
+      <div className="relative z-0 min-h-screen w-full overflow-x-hidden pb-24">
+
+        <div className="flex justify-center">
+          <div className="flex-grow-1 w-full max-w-8xl overflow-x-auto sm:mx-4 md:mx-[30px] lg:mx-11 scrollbar-thin scrollbar-track-canvas-95 scrollbar-thumb-canvas-80 scrollbar-thumb-rounded-full">
+            <div className="relative z-10 overflow-hidden pb-4">
+              <div className="absolute -z-10 block size-full max-w-none bg-contain bg-center bg-no-repeat opacity-30 md:hidden" style={{ backgroundImage: "url('https://assets.blast.tv/images/teams/f239109c-ffef-4e83-830e-5ca340ee432b')" }}></div>
+              <div className="flex h-56 w-full items-center gap-5 pt-16 md:h-80">
+                <div className="flex place-content-center items-center lg:w-[21.5rem]">
+                  <div className="relative flex size-32 shrink-0 overflow-hidden rounded-full md:size-48">
+                    <img className="absolute h-52 max-w-none md:h-72" src="" alt="" />
+                  </div>
+                </div>
+                <div className="flex h-full flex-col place-content-center items-start gap-2 md:w-96 md:gap-6">
+                  <h1 className="font-style-desktop-h2 md:font-style-desktop-h1">ÈoGustta</h1>
+                  <div className="flex items-center gap-2">
+                    <img src="/br.svg" alt="BR" style={{ width: '25px', height: '18.75px' }} />
+                    <span className="font-style-subheading-4 md:font-style-subheading-2">Gustavo Ferreira Martins</span>
+                  </div>
+                  <div className="hidden w-full items-center justify-between text-neutral-10 font-style-b2-body-copy md:flex">
+                    <div className="flex gap-2 text-neutral-20">Age: <span className="font-bold text-neutral-10">21 Years old</span></div>
+                    <div className="flex gap-2 text-neutral-20">Team: <span className="font-bold capitalize text-neutral-10"><a className="text-yellow no-underline" href="/teams/complexity"> Anonymous</a></span></div>
+                    <div className="flex gap-2 text-neutral-20">Status: <span className="font-bold capitalize text-neutral-10">Starter</span></div>
+                  </div>
+                </div>
+                <div className="hidden h-[27.5rem] opacity-30 md:block">
+                  <img className="absolute -top-16 right-0 size-[34.5rem] select-none" src="https://assets.blast.tv/images/teams/f239109c-ffef-4e83-830e-5ca340ee432b" />
+                </div>
+              </div>
+              <div className="flex w-full items-center justify-between text-neutral-20 font-style-b3-body-copy md:hidden">
+                <div className="flex gap-1 text-neutral-20">Age: <span className="text-neutral-20">21 Years old</span></div>
+                <div className="flex gap-1 text-neutral-20">Team: <span className="capitalize text-neutral-20">Anonymous</span></div>
+                <div className="flex gap-1 text-neutral-20">Status: <span className="font-bold capitalize">Starter</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+       </div>
+
+    </div>
+
+  );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Background } from "./components/background";
+import { PlayerProfile } from "./components/playerprofile";
 
 export default function Home() {
     return (
         <main>
-            <Background />
+            <Background/>
+            <PlayerProfile/>
         </main>
     );
 }
