cee11ce6a5d7eaa387d2cced39455aba44e4e340
GustavoFerMartins <60547697+Gustavo3022020@users.noreply.github.com>
2024-03-06 11:28:04 -0300

    test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,11 @@
-import { Player } from "./pages/player/player";
 import { Players } from "./pages/player/players";
 import { Assertive } from "./components/assertive/assertive";
-
 
 export default function Home() {
     return (
         <main>
-
-            <Assertive/>
-            <Players/>
-
+            <Assertive />
+            <Players />
         </main>
     );
 }
