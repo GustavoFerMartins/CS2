8537d85e7f2845ff88bfead993905d1245cf67e0
GustavoFerMartins <60547697+Gustavo3022020@users.noreply.github.com>
2024-03-07 11:51:04 -0300

    Update

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    testPathIgnorePatterns: ['/node_modules/'],
+    testEnvironment: "jsdom",
+    testPathIgnorePatterns: ["/node_modules/", "/.next/"],
     collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.js'],
-    setupFilesAfterEnv: ["<rootDir>/.jest/setup.js"]
-}
+    collectCoverageFrom: ["src/**/*.ts(x)?", "!src/**/stories.tsx"],
+    setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
+    modulePaths: ["<rootDir>/src/"],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+    },
+};
